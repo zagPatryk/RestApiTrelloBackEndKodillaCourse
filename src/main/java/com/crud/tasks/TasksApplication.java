ec8c8752a4f4d9824b22f0a11c475c98abf82dd5
zagPatryk <zag.patryk@gmail.com>
2020-09-02 19:46:40 +0200

    Heroku update fix

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,26 +6,24 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-
-//@SpringBootApplication/*(scanBasePackages={"com.crud.tasks.repository","com.crud.tasks.service"
-//        ,"com.crud.tasks.controller","com.crud.tasks.domain","com.crud.tasks.mapper"})*/
-//public class TasksApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(TasksApplication.class, args);
-//    }
-//}
-
-
-
-
-@SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+// heroku
+@SpringBootApplication/*(scanBasePackages={"com.crud.tasks.repository","com.crud.tasks.service"
+        ,"com.crud.tasks.controller","com.crud.tasks.domain","com.crud.tasks.mapper"})*/
+public class TasksApplication {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TasksApplication.class);
-    }
 }
+
+// local
+//@SpringBootApplication
+//public class TasksApplication extends SpringBootServletInitializer {
+//    public static void main(String[] args) {
+//        SpringApplication.run(TasksApplication.class, args);
+//    }
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(TasksApplication.class);
+//    }
+//}
