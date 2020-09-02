e1705d9793e1a10566575364aa1ba197c830537e
zagPatryk <62852383+zagPatryk@users.noreply.github.com>
2020-09-02 21:04:52 +0200

    Update CoreConfiguration.java

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -25,7 +25,6 @@ public class CoreConfiguration implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-
                 .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
