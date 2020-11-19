da5d7c787bdab4d60fc93e6b3e828de2d47585df
zagPatryk <zag.patryk@gmail.com>
2020-11-19 18:10:17 +0100

    Local version to git

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,24 +3,26 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-// heroku
-@SpringBootApplication
-public class TasksApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TasksApplication.class, args);
-    }
-}
-
-//// local
+//// heroku
 //@SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication {
 //    public static void main(String[] args) {
 //        SpringApplication.run(TasksApplication.class, args);
 //    }
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(TasksApplication.class);
-//    }
 //}
+
+// local
+@SpringBootApplication
+public class TasksApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TasksApplication.class);
+    }
+}
